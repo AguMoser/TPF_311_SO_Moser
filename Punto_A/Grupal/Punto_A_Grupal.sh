2e0ebb06fee509331f2bcf582ce640932e31fb83
Sysadmin <agumoser+sysadmin@gmail.com>
2026-06-12 23:13:23 +0000

    Punto A grupal: agrega script de continentes

---
+++ Punto_A/Grupal/Punto_A_Grupal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Sysadmin
+mkdir -p Continentes/America/{Uruguay/{Montevideo,Paysandu,Salto},Peru/{Junin,San_Martin},Colombia/Bogota}
+
+# Manager
+mkdir -p Continentes/Europa/{Italia/{Roma,Milan,Napoles},Holanda/{Amsterdam,Rotterdam},Suiza/Zurich}
+
+# Dev
+mkdir -p Continentes/Asia/{Japon/{Tokio,Osaka,Kioto},India/{Goa,Kerala},Corea_del_Sur/Busan}
